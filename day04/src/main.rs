79fbe774d8c7813f676ce738712a043f65c32b33
Alex Slover <slover.alex@gmail.com>
2020-12-05 15:18:25 -0800

    Finish day 4

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,30 +1,5 @@
 use std::{fs::File, io::{self, BufRead, BufReader}};
-
-// struct Passport {
-//     byr: Option<i32>,
-//     iyr: Option<i32>,
-//     eyr: Option<i32>,
-//     hgt: Option<i32>,
-//     hcl: Option<String>,
-//     ecl: Option<String>,
-//     pid: Option<String>,
-//     cid: Option<String>
-// }
-
-// impl Passport {
-//     fn new() -> Passport {
-//         Passport {
-//             byr: None,
-//             iyr: None,
-//             eyr: None,
-//             hgt: None,
-//             hcl: None,
-//             ecl: None,
-//             pid: None,
-//             cid: None
-//         }
-//     }
-// }
+use regex::Regex;
 
 static FIELDS: [&str; 8] = ["cid", "pid", "ecl", "hcl", "hgt", "eyr", "iyr", "byr"];
 
@@ -32,6 +7,14 @@ fn main() {
     let file = File::open("input.txt").unwrap();
     let mut valid_passports = 0;
     let mut has_fields: u8 = 0;
+
+    let BYR: Regex = Regex::new(r"^\d{4}$").unwrap();
+    let IYR: Regex = Regex::new(r"^\d{4}$").unwrap();
+    let EYR: Regex = Regex::new(r"^\d{4}$").unwrap();
+    let HGT: Regex = Regex::new(r"^(?P<val>\d+)(?P<unit>cm|in)$").unwrap();
+    let HCL: Regex = Regex::new(r"^#[0-9a-fA-F]{6}$").unwrap();
+    let ECL: Regex = Regex::new(r"^(amb|blu|brn|gry|grn|hzl|oth)$").unwrap();
+    let PID: Regex = Regex::new(r"^\d{9}$").unwrap();
      
     for line_ref in io::BufReader::new(file).lines() {
         let line = line_ref.unwrap();
@@ -50,11 +33,53 @@ fn main() {
         for field in line.split_ascii_whitespace() {
             let parts: Vec<&str> = field.splitn(2, ':').collect();
             let k = parts[0];
+            let v = parts[1];
 
-            if k != "cid" { 
-                if let Some(i) = FIELDS.iter().position(|&f| f == k) {
+            if let Some(i) = FIELDS.iter().position(|&f| f == k) {
+                let valid = match k {
+                    "byr" => {
+                        if BYR.is_match(v) {
+                            let year = v.parse::<i32>().unwrap();
+                            year >= 1920 && year <= 2002
+                        } else {
+                            false
+                        }
+                    },
+                    "iyr" => {
+                        if IYR.is_match(v) {
+                            let year = v.parse::<i32>().unwrap();
+                            year >= 2010 && year <= 2020
+                        } else {
+                            false
+                        }
+                    },
+                    "eyr" => {
+                        if EYR.is_match(v) {
+                            let year = v.parse::<i32>().unwrap();
+                            year >= 2020 && year <= 2030
+                        } else {
+                            false
+                        }
+                    },
+                    "hgt" => {
+                        HGT.captures(v).map(|cap| {
+                            let val = cap.name("val").unwrap().as_str().parse::<i32>().unwrap();
+                            if cap.name("unit").unwrap().as_str() == "cm" {
+                                val >= 150 && val <= 193
+                            } else {
+                                val >= 59 && val <= 76
+                            }
+                        }).unwrap_or(false)
+                    },
+                    "hcl" => HCL.is_match(v),
+                    "ecl" => ECL.is_match(v),
+                    "pid" => PID.is_match(v),
+                    "cid" => false,
+                    _ => false
+                };
+                print!("{}: {}", k, valid);
+                if valid {
                     has_fields += 1 << i;
-                    print!(" field {}, has_fields = {}   ", k, has_fields);
                 }
             }
 
